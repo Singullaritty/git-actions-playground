369a0afa7e48e348360ad9b14b9e1fd53158ecb1
Bohdan <bohdan.leshchenko1@gmail.com>
2023-06-04 20:40:30 +0200

    typo fix

--- k8s-deploy.sh
+++ k8s-deploy.sh
@@ -2,7 +2,7 @@
 
 #k8s-deployment.sh
 
-sed -i "s#to_be_replaced#${imageName}#g" k8s_deployment_service.yaml
+sed -i "s#to_be_replaced#${imageName}#g" k8s_deployment.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
 if [[ $? -ne 0 ]]; then
